70b63c936eb39dd0cbdf9ae7fd73d4595c3e4c82
Erik Edman <61149651+ErikEdman1996@users.noreply.github.com>
2025-10-28 10:29:51 +0100

    First commit

---
+++ src/main/java/org/example/edufy_userservice/EdufyUserserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.edufy_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyUserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyUserserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/edufy_userservice/EdufyUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.edufy_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyUserserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
